295b11f16a9392b381d7ac8d3729f6447f5a821b
Nishanth Shetty <nishanthspshetty@gmail.com>
2019-11-14 01:59:45 +0530

    first level lein project generation and main function written, generated project executed

--- src/generator/core.clj
+++ src/generator/core.clj
@@ -1,8 +1,9 @@
-(ns 'generator.core
-  (:require ['generator.setup]))
+(ns generator.core
+  (:require [generator.setup]
+            [generator.coder]))
 
 ;;supported operator list
-(defn operator #{:add :subtract :multiply :divide})
+(def operator #{:add :subtract :multiply :divide})
 
 (defn to-op-type
   [op-str]
@@ -18,20 +19,28 @@
 (defn generate-code
   "I will generate code!!!"
   [arg-tokens]
-  (let [[operator-str start-num end-num] arg-tokens
-        [operator (to-op-type operator-str)]]
-
+  (let
+   [[operator-str start-num end-num] arg-tokens
+    operator (to-op-type operator-str)]
+    (println operator-str " " start-num " " end-num)
     ;;check if operators supported yet
-    (if (= operator :unknown)
-      (str "I dont support " operator-str " yet, master is working on it, I shall support this soon"))
+    (cond (= operator :unknown)
+          (str "I dont support " operator-str " yet, master is working on it, I shall support this soon"))
 
-    (if (= start-num end-num)
-      (str "range ended even before I began. Start : " start ", End : " end-num))
+    (cond (= start-num end-num)
+          (str "range ended even before I began. Start : " start-num ", End : " end-num))
     ;; all good for generator to writing...
 
     (try
-      (generator.setup/set-it-up operator operator-str start-num end-num)
-      (write-code)
-      (write-test) ;;huhh, this getting serious
-      (catch e
-             (str "I have let you down! Apologies [Cause " (.getMessage e) "]")))))
+      (println "starting generator ")
+      (let [root-dir
+            (generator.setup/set-it-up operator operator-str start-num end-num)]
+        (println "Generating code...")
+        (generator.coder/write-code root-dir operator  operator-str start-num end-num)
+        (println "Generating tests...")
+        (generator.coder/write-test root-dir operator  operator-str start-num end-num)
+        (str "programmer ran Successfully , check projects `" root-dir "` and read README to know how to use."))
+
+      (catch Exception e
+        (throw e)
+        (str "I have let you down! Apologies [Cause " (.getMessage e) "]")))))
--- src/programmer/core.clj
+++ src/programmer/core.clj
@@ -1,19 +1,26 @@
 (ns programmer.core
   (:gen-class)
-  (:require ['generator.core]))
+  (:require [generator.core]))
+
+(defn to-number [num-str]
+  (try
+    (Integer. num-str)
+    (catch Exception e 0)))
 
 
 (defn parse_arguments
   "parse argument string to its corresponding type value"
   [args]
-  (let [operator-str start-str end-str (break-it args)]
+  (let [[operator-str start-str end-str] args]
     [operator-str (to-number start-str) (to-number end-str)]))
 
 (defn -main
   "starting the programmer..."
   [& args]
   (if (< (count args) 3)
-    (println "I do not how to proceed without giving me any arguments yet, later I might change my code and patch myself, until then, \nHere, this is how you use me \n" "<programmer> arithmetic_operator start end\n")
+    (println "I do not know how to proceed without giving me any arguments yet, later I might change my code and patch myself, until then, \nHere, this is how you use me \n" "<programmer> arithmetic_operator start end\n")
+    (println
     (-> args
         (parse_arguments)
         (generator.core/generate-code))))
+)
