713f59097711a2280c2695b57306188376556b0e
Nishanth Shetty <nishanthspshetty@gmail.com>
2019-11-14 13:21:00 +0530

    apply some formattimng to generated code

--- add_between_1_and_10/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject add_between_1_and_10"0.1.0-RELEASE"
-description "FIXME: write description"
-:url "http://example.com/FIXME"
-:license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-:url "https://www.eclipse.org/legal/epl-2.0/"}
-:dependencies [[org.clojure/clojure "1.10.0"]]
-:main ^:skip-aot main.core
-:target-path "target/%s"
-:profiles {:uberjar {:aot :all}})
